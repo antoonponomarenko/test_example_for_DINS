99941bbfb3f859894c6f3261f9815b2f772dc5e6
antoonponomarenko <antoonponomarenko@yandex.ru>
2019-09-02 20:16:17 +0300

    add 12  test case

--- 12_download_size_files_test_case  .py
+++ 12_download_size_files_test_case  .py
@@ -3,7 +3,8 @@ from ftplib import FTP
 import os
 
 test_file_name = '2MB.zip'
-dir_for_download = './'
+expect_result = 2097152
+dir_for_download = '.'
 
 ftp = FTP('speedtest.tele2.net')
 status_connection = ftp.login()
@@ -12,14 +13,14 @@ local_file_name = os.path.join(dir_for_download, test_file_name)
 lf = open(local_file_name, "wb")
 ftp.retrbinary("RETR " + test_file_name, lf.write, 8*1024)
 lf.close()
-result = os.path.exists(test_file_name)
-file = open(test_file_name)
-file.close()
+
+size_download_file = os.stat(test_file_name).st_size
+
 
 class Download_file_test_case(unittest.TestCase):
     def test_connection(self):
-        actual_result = result
-        expected_result = True
+        actual_result = size_download_file
+        expected_result = expect_result
         self.assertEqual(actual_result, expected_result, )
 
 if __name__ == '__main__':
--- 13_download_name_files_test_case   (copy).py
+++ 13_download_name_files_test_case   (copy).py
@@ -3,7 +3,8 @@ from ftplib import FTP
 import os
 
 test_file_name = '2MB.zip'
-dir_for_download = './'
+expect_result = '2MB.zip'
+dir_for_download = '.'
 
 ftp = FTP('speedtest.tele2.net')
 status_connection = ftp.login()
@@ -12,11 +13,20 @@ local_file_name = os.path.join(dir_for_download, test_file_name)
 lf = open(local_file_name, "wb")
 ftp.retrbinary("RETR " + test_file_name, lf.write, 8*1024)
 lf.close()
-result = os.path.exists(test_file_name)
-file = open(test_file_name)
-file.close()
 
-class Download_file_test_case(unittest.TestCase):
+list_name_download_file = [f for f in os.listdir(dir_for_download) ]
+result = False
+
+print(list_name_download_file)
+
+
+if expect_result in list_name_download_file:
+    result = True
+
+
+
+
+class Download_name_file_test_case(unittest.TestCase):
     def test_connection(self):
         actual_result = result
         expected_result = True
